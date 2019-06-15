2f28a9fefb8ac5a32e69c4a79c168e9bbc57d0e7
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2019-06-16 03:10:53 +0500

    fix board root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'web/board#show'
+  root to: 'web/boards#show'
 
   scope module: :web do
     resource :board, only: :show
